53adfc2da72a77012e55ca7bbfc64a32ad96f614
nazdravi412 <81826403+nazdravi412@users.noreply.github.com>
2022-10-14 17:42:23 -0400

    Update fxmanifest.lua
    chore: update version and description

--- DrugTrafficking/fxmanifest.lua
+++ DrugTrafficking/fxmanifest.lua
@@ -2,9 +2,9 @@ fx_version 'bodacious'
 game 'gta5'
 
 name 'DrugTrafficking'
-description 'Drug Trafficking Job for ND_Framework'
-author 'Lama#9612'
-version '1.0'
+description 'Drug Trafficking Job for ND_Framework and Ox_Inventory'
+author 'Lama#9612. Edited by Nazdravi412'
+version '2.0'
 
 client_script 'client.lua'
 server_script 'server.lua'
